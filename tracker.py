4756aff424323d51e518222688017ed2ba1e02c2
rj14ng <rocco.jiang5@gmail.com>
2019-07-12 14:30:24 +0800

    Update tracker.py
    Now uses webcam capture and detects pupils (but not well)
    More calibration is needed

--- tracker.py
+++ tracker.py
@@ -3,6 +3,16 @@
 import cv2
 import numpy as np
 
+# Initialise haar cascade classifiers
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+
+# Initialise blob detection algorithm
+detector_params = cv2.SimpleBlobDetector_Params()
+detector_params.filterByArea = True
+detector_params.maxArea = 1500  # Area filtering for better results
+detector = cv2.SimpleBlobDetector_create(detector_params)
+
 # Detect face
 def detect_face(img, cascade):
     # Make image frame grey
@@ -64,16 +74,59 @@ def detect_eyes(img, cascade):
     
     return left_eye, right_eye
 
+# Cut our eyebrows from eye frame
+def cut_eyebrows(img):
+    height, width = img.shape[:2]
+    eyebrow_h = int(height / 4)  # Eyebrows take up ~25% of image starting from top
+    img = img[eyebrow_h: height, 0: width]  # Cut eyebrows out
+
+    return img
+
+# Detect and draw blobs on frames
+def blob_process(img, threshold, detector):
+    frame_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(
+        frame_grey,
+        thresh = threshold,
+        maxval = 255,
+        type = cv2.THRESH_BINARY
+    )  # _ is a throwaway variable
+    
+    # Reduce 'noise'
+    img = cv2.erode(img, None, iterations = 2)
+    img = cv2.dilate(img, None, iterations = 4)
+    img = cv2.medianBlur(img, 5)
+
+    keypoints = detector.detect(img)
+    return keypoints
+
+# Trackbar requires a function to happen on every movement
+# Create a function that does nothing
+def nothing(x):
+    pass
+
 def main():
-    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
-    img = cv2.imread("test_image.jpg")
+    cap = cv2.VideoCapture(0)  # Camera capture
+    cv2.namedWindow("Webcam capture")
+    cv2.createTrackbar("Threshold", "Webcam capture", 0, 255, nothing)
 
-    detect_face(img, face_cascade)
-    detect_eyes(img, eye_cascade)
-
-    cv2.imshow("my image", img)
-    cv2.waitKey(0)
+    while True:
+        _, frame = cap.read()  # _ is a throwaway variable
+        face_frame = detect_face(frame, face_cascade)
+        if face_frame is not None:  # Prevent crash if face not detected
+            eyes = detect_eyes(face_frame, eye_cascade)
+            for eye in eyes:
+                if eye is not None:  # Prevent crash if eye not detected
+                    threshold = cv2.getTrackbarPos("Threshold", "Webcam capture")
+                    eye = cut_eyebrows(eye)  # Cut out eyebrows from eye frame
+                    keypoints = blob_process(eye, threshold, detector)
+                    eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        
+        cv2.imshow("Webcam capture", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
